9121a0f00ee1283175f3b7808a714f7bb7e2555c
Aditya Gusain <adisandi3112002@gmail.com>
2021-11-26 14:04:30 +0530

    Rectified Code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Row } from "antd";
+import "antd/dist/antd.css";
+import "./App.css";
+import AdminUI from "./Components/index";
+
+function App() {
+  return (
+    <div className="admin-wrp">
+      <Row justify="center" align="middle">
+        <AdminUI />
+      </Row>
+    </div>
+  );
+}
+
+export default App;
